35d4d1ac0183ceb4fc2828e9d4ddd62ec2884f3d
Don Li <yli877@aucklanduni.ac.nz>
2023-08-30 15:05:08 +1200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+// let myVariable = "Bob";
+// myVariable = "Steve";
+
+// // alert("hello!");
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+//   });
+
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/goliath-bird-eater-tarantula.avif") {
+    myImage.setAttribute("src", "images/spiders.avif");
+  } 
+  else {
+    myImage.setAttribute("src", "images/goliath-bird-eater-tarantula.avif");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
